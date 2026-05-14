29d37d735a54962788ea840bb8df25256f1c09b6
carlosdhavid <cdhavidsp44@educastur.es>
2026-05-14 17:09:26 +0000

    Actualización del proyecto: tests, build.gradle y pipeline

---
+++ src/test/java/com/ciberCDSP/calculatorCDSP/CalculatorApplicationTests.java
@@ -0,0 +1,21 @@
+package com.ciberCDSP.calculatorCDSP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class CalculatorApplicationTests {
+
+    private Calculator calculator = new Calculator();
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    public void testSum() {
+        assertEquals(5, calculator.sum(2, 3));
+    }
+}
+
