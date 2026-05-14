93ee1d2592c25f2697a7036a35cfc005573be962
carlosdhavid <cdhavidsp44@educastur.es>
2026-05-14 14:44:55 +0000

    Añadido proyecto Spring Boot con Gradle (calculatorCDSP)

---
+++ src/main/java/com/ciberCDSP/calculatorCDSP/CalculatorCdspApplication.java
@@ -0,0 +1,13 @@
+package com.ciberCDSP.calculatorCDSP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorCdspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorCdspApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ciberCDSP/calculatorCDSP/CalculatorCdspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciberCDSP.calculatorCDSP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorCdspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
