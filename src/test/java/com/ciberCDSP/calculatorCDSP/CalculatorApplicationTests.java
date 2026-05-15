22623f15c2ed146fe99022a3f6d7648060b9089a
carlosdhavid <cdhavidsp44@educastur.es>
2026-05-15 04:52:09 +0000

    Actualización del proyecto

--- src/test/java/com/ciberCDSP/calculatorCDSP/CalculatorApplicationTests.java
+++ src/test/java/com/ciberCDSP/calculatorCDSP/CalculatorApplicationTests.java
@@ -1,15 +1,17 @@
 package com.ciberCDSP.calculatorCDSP;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class CalculatorApplicationTests {
 
     @Test
-    void testSumar() {
+    void testAdd() {
         Calculator calculator = new Calculator();
-        assertEquals(5, calculator.sumar(2, 3));
+        assertEquals(5, calculator.add(2, 3));
     }
 }
+
 
 
