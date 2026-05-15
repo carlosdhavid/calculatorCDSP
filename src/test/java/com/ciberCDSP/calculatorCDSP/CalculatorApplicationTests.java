2fd469d3cccc7e6ebfdc444ef7e55bba65338a37
carlosdhavid <cdhavidsp44@educastur.es>
2026-05-15 02:08:02 +0000

    Eliminar Spring Boot y añadir test JUnit simple

--- src/test/java/com/ciberCDSP/calculatorCDSP/CalculatorApplicationTests.java
+++ src/test/java/com/ciberCDSP/calculatorCDSP/CalculatorApplicationTests.java
@@ -1,21 +1,15 @@
 package com.ciberCDSP.calculatorCDSP;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
-@SpringBootTest
 public class CalculatorApplicationTests {
 
-    private Calculator calculator = new Calculator();
-
     @Test
-    void contextLoads() {
-    }
-
-    @Test
-    public void testSum() {
-        assertEquals(5, calculator.sum(2, 3));
+    void testSumar() {
+        Calculator calculator = new Calculator();
+        assertEquals(5, calculator.sumar(2, 3));
     }
 }
+
 
